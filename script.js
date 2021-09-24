6a1d3b8c5cdbd6cd7e19e6b09533d447733b89df
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2021-09-24 10:25:26 +0200

    js fetch
    fetch ok

--- script.js
+++ script.js
@@ -1,79 +1,29 @@
-/*var name = "Mimi Keel";
-var country = "UK";
-
-a = '<div class="photographes">'${name}'</div><div>'${country}'</div> <img src="mimikeel.jpg">'
-
-document.querySelector(".toto").innerHTML(a);*/
-
-/*function photographersList() {
-    fetch("photographers.json")
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(value) {
-      document
-          .getElementById("gallery")
-          .innerText = value.queryString.name;
-    })
-    .catch(function(err) {
-      // Une erreur est survenue
-    });
-  }
-document
-  .getElementById("tags-input")
-  .addEventListener("click", photographersList);
-
-
-const gallery = document.getElementById("gallery");
-const div = document.createElement("div");
-gallery.append(div);
-div.innerHTML = photographers.name;
-
-const photographerName = document.createElement("photographerName");
-photographerName.innerText = photographers.name;
-div.append(); */
-
-
-/*function getJSON("photographers.json") {
-    return get("photographers.json").then(JSON.parse);
+let photographers = "";
+fetch("photographers.json")
+.then(function(response) {
+  if(response.ok) {
+    return response.json();
 }
+})
 
-function showPhotographers(photographer) {
-    const gallery = document.getElementById("gallery");
-    const div = document.createElement("div");
-    div.innerHTML = photographers;
-    return gallery.appendChild(div);
-}*/
+.then(function(jsonObj) {
+  //console.log(jsonObj);
+  //console.log(jsonObj["photographers"]);
+  let photographers = jsonObj["photographers"];
 
+  let tag = photographers.tags;
 
-const gallery = document.getElementById("gallery");
-const newElt = document.createElement("div");
-gallery.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
-gallery.appendChild(newElt); 
-newElt.innerHTML = "hey";
+  photographers.forEach(function(photographer) {
+    console.log(photographer);
+    let htmlContent = document.createElement("div");
+    htmlContent.classList.add("photographe");
 
-fetch("/photographers.json")
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
+    //htmlContent.innerHTML = "<img src=" + photographer["img"] + "/>"photographer["name"];
+
+    document.getElementById("gallery").appendChild(htmlContent);
+
+    console.log(htmlContent);
   })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-
-  });
-
-
-const input = document.querySelector('input');
-const log = document.getElementById('values');
-
-input.addEventListener('input', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
+    
+});
 
