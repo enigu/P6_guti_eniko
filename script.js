aa96c41c335df39c0b5dbea1e5dbf941a49e8cee
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2021-09-29 16:45:31 +0200

    photografers.forEach
    code added - avatar does not print out

--- script.js
+++ script.js
@@ -5,25 +5,63 @@ fetch("photographers.json")
     return response.json();
 }
 })
-
 .then(function(jsonObj) {
   //console.log(jsonObj);
   //console.log(jsonObj["photographers"]);
   let photographers = jsonObj["photographers"];
-
-  let tag = photographers.tags;
+  //let tag = photographers.tags;
 
   photographers.forEach(function(photographer) {
-    console.log(photographer);
+    //photographer.img
+    let imgPhotographer = document.createElement("img");
+    imgPhotographer.classList.add("photo");
+    let gallery = document.getElementById("gallery");
+    gallery.appendChild(imgPhotographer);
+    imgPhotographer.innerHTML = `<a href="#"> <img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"> </a>`;
+    console.log(imgPhotographer);
+
+    // photographer.name
     let htmlContent = document.createElement("div");
     htmlContent.classList.add("photographe");
-
-    //htmlContent.innerHTML = "<img src=" + photographer["img"] + "/>"photographer["name"];
-
-    document.getElementById("gallery").appendChild(htmlContent);
-
+    gallery.appendChild(htmlContent);
+    htmlContent.innerHTML = `<a href="#"> <h2>${photographer.name}</h2> </a>`;
     console.log(htmlContent);
-  })
-    
-});
+
+    //photographer.location
+    let contentLocation = document.createElement("p");
+    contentLocation.classList.add("location");
+    gallery.appendChild(contentLocation);
+    contentLocation.innerHTML = `<a href="#">${photographer.city}, ${photographer.country}</a>`;
+    console.log(contentLocation);
+
+    //photographer.tagline
+    let tagLine = document.createElement("p");
+    tagLine.classList.add("tagline");
+    gallery.appendChild(tagLine);
+    tagLine.innerHTML = `<p>${photographer.tagline}</p>`;
+    console.log(tagLine);
+
+    //photographer.price
+    let priceOfShooting = document.createElement("p");
+    priceOfShooting.classList.add("price");
+    gallery.appendChild(priceOfShooting);
+    priceOfShooting.innerHTML = `<p>${photographer.price}€/jour</p>`;
+    console.log(priceOfShooting);
+
+    //photographer.tags
+    let photographerTags = document.createElement("p");
+    photographerTags.classList.add("tags");
+    gallery.appendChild(photographerTags);
+    photographerTags.innerHTML = `<p>#${photographer.tags}</p>`;
+    console.log(photographerTags);
+
+  }) 
+})
+
+
+
+
+
+
+
 
