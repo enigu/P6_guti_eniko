69a7c4e077285ec391c190d57c6a352c786d5966
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2021-10-01 10:23:40 +0200

    index.html - affichage photographers
    JS affichage photographers ok

--- script.js
+++ script.js
@@ -6,57 +6,76 @@ fetch("photographers.json")
 }
 })
 .then(function(jsonObj) {
-  //console.log(jsonObj);
+  //let tags = [""];
+
+  generatePhotographers(jsonObj["photographers"]);
+  //addTags();
+
+})
+
+
+
+function generatePhotographers(photographers) {
+    //console.log(jsonObj);
   //console.log(jsonObj["photographers"]);
-  let photographers = jsonObj["photographers"];
   //let tag = photographers.tags;
 
   photographers.forEach(function(photographer) {
-    //photographer.img
-    let imgPhotographer = document.createElement("img");
-    imgPhotographer.classList.add("photo");
+  
+    let photographerDiv = document.createElement("div");
     let gallery = document.getElementById("gallery");
-    gallery.appendChild(imgPhotographer);
-    imgPhotographer.innerHTML = `<a href="#"> <img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"> </a>`;
-    console.log(imgPhotographer);
 
-    // photographer.name
-    let htmlContent = document.createElement("div");
-    htmlContent.classList.add("photographe");
-    gallery.appendChild(htmlContent);
-    htmlContent.innerHTML = `<a href="#"> <h2>${photographer.name}</h2> </a>`;
-    console.log(htmlContent);
+    photographerDiv.classList.add("photographe");
 
-    //photographer.location
-    let contentLocation = document.createElement("p");
-    contentLocation.classList.add("location");
-    gallery.appendChild(contentLocation);
-    contentLocation.innerHTML = `<a href="#">${photographer.city}, ${photographer.country}</a>`;
-    console.log(contentLocation);
-
-    //photographer.tagline
-    let tagLine = document.createElement("p");
-    tagLine.classList.add("tagline");
-    gallery.appendChild(tagLine);
-    tagLine.innerHTML = `<p>${photographer.tagline}</p>`;
-    console.log(tagLine);
-
-    //photographer.price
-    let priceOfShooting = document.createElement("p");
-    priceOfShooting.classList.add("price");
-    gallery.appendChild(priceOfShooting);
-    priceOfShooting.innerHTML = `<p>${photographer.price}€/jour</p>`;
-    console.log(priceOfShooting);
-
-    //photographer.tags
-    let photographerTags = document.createElement("p");
-    photographerTags.classList.add("tags");
-    gallery.appendChild(photographerTags);
-    photographerTags.innerHTML = `<p>#${photographer.tags}</p>`;
-    console.log(photographerTags);
-
+    //html.innerHTML = '<img src="./Sample Photos/Photographers ID Photos/' + photographer["portrait"] + '"><div class="name">' + photographer["name"] + '</div>';
+    photographerDiv.innerHTML = `<img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"><div class="name">${photographer.name}</div><a href="photographer.html?id=${photographer.id}">Coucou</a><a href="#">${photographer.city}, ${photographer.country}</a><p>${photographer.tagline}</p><p>${photographer.price}€/jour</p><p>#${photographer.tags}</p>`;
+    //le lien avec l'id après le ? qui permet, sur le fichier js de ta page de profil d'utiliser la fonction url.parse pour récupérer les variables
+    gallery.appendChild(photographerDiv);
   }) 
-})
+}
+
+
+
+
+/*<div class="tag"></div>
+<span>javascript</span>
+<i class="fas fa-hashtag"></i>*/
+
+/*const tagContainer = document.querySelector('.tag-container');
+const input = document.querySelector('.tag-container input');
+
+function createTag(label) {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'tag');
+  const span = document.createElement('span');
+  span.innerHTML = label ;
+  //const hashTag = document.createElement('i');
+  //hashTag.setAttribute('class', 'fas')
+
+  div.appendChild(span);
+  //div.appendChild(hashTag);
+  return div;
+}
+
+//tagContainer.prepend(createTag('javascript'))
+//var tags = []
+
+function addTags() {
+  var tags = [""]
+
+  const tags.forEach(function(tag) {
+    const tag = createTab(input.value);
+    tagContainer.prepend(tag);
+  });
+}
+
+input.addEventListener('keyup', function(e) {
+  if(e.key === 'Enter') {
+    tags.push(input.value);
+    input.value = '';
+  }
+
+})*/
 
 
 
