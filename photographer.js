2c3d8c79b84ec9aad1bc9438881a6bc4f12368dc
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2021-10-27 09:54:32 +0200

    html modal - photographer
    ok

--- photographer.js
+++ photographer.js
@@ -1,4 +1,4 @@
-//fetch de json
+//fetch de json photographer pour la div info de la section photographers-profile
 fetch("photographers.json")
   .then(function(response) {
     if (response.ok) {
@@ -19,17 +19,28 @@ fetch("photographers.json")
         if (photographer["id"]==photographerId) {
           let photographerDiv = document.createElement("div");
           photographerDiv.classList.add("photographe");
-          let avatarDiv = document.createElement("div")
+          let button = document.createElement("button");
+          button.classList.add("contactme");
+          let avatarDiv = document.createElement("div");
           avatarDiv.classList.add("avatar");
           let info = document.getElementById("info");
 
-          avatarDiv.innerHTML = `<div><img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"</div>`;
           photographerDiv.innerHTML = `<div><a class="name">${photographer.name}</a><br>
           <a href="photographer.html?id=${photographer.id}">${photographer.city}, ${photographer.country}</a>
           <p>${photographer.tagline}</p><p>${photographer.price}€/jour</p><p>#${photographer.tags}</p></div>`;
+          button.innerHTML = "Contactez-moi";  
+          avatarDiv.innerHTML = `<div><img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"</div>`;
+
           info.appendChild(photographerDiv);
+          info.appendChild(button);
           info.appendChild(avatarDiv);
+          
         }
     })  
   })
+
+
+  // fetch  de la section media des photographes
+
+
   
