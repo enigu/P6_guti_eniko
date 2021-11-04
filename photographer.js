78ef99e56e01c072568aa333bbaee47d6c8d6efb
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2021-11-04 16:57:58 +0100

    js totalCounter of likes
    to be corrected

--- photographer.js
+++ photographer.js
@@ -17,88 +17,79 @@ fetch("photographers.json")
     //console.log(photographerId);
 
     photographers.forEach(function(photographer) {
-        if (photographer["id"]==photographerId) {
-            currentPhotographer = photographer;
+      if (photographer["id"]==photographerId) {
+        currentPhotographer = photographer;
 
-          let photographerDiv = document.createElement("div");
-          photographerDiv.classList.add("photographe");
-          let button = document.createElement("button");
-          //button.classList.add("contactme");
-          button.setAttribute("id", "contactme");
-          let avatarDiv = document.createElement("div");
-          avatarDiv.classList.add("avatar");
-          let info = document.getElementById("info");
+        let photographerDiv = document.createElement("div");
+        photographerDiv.classList.add("photographe");
+        let button = document.createElement("button");
+        //button.classList.add("contactme");
+        button.setAttribute("id", "contactme");
+        let avatarDiv = document.createElement("div");
+        avatarDiv.classList.add("avatar");
+        let info = document.getElementById("info");
 
-          photographerDiv.innerHTML = `<div><a class="name">${photographer.name}</a><br>
-          <a href="photographer.html?id=${photographer.id}">${photographer.city}, ${photographer.country}</a>
-          <p>${photographer.tagline}</p><p>${photographer.price}€/jour</p><p>#${photographer.tags}</p></div>`;
-          button.innerHTML = "Contactez-moi";  
-          avatarDiv.innerHTML = `<div><img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"</div>`;
+        photographerDiv.innerHTML = `<div><a class="name">${photographer.name}</a><br>
+        <a href="photographer.html?id=${photographer.id}">${photographer.city}, ${photographer.country}</a>
+        <p>${photographer.tagline}</p><p>${photographer.price}€/jour</p><p>#${photographer.tags}</p></div>`;
+        button.innerHTML = "Contactez-moi";  
+        avatarDiv.innerHTML = `<div><img src="./Sample Photos/Photographers ID Photos/${photographer.portrait}"</div>`;
 
-          info.appendChild(photographerDiv);
-          info.appendChild(button);
-          info.appendChild(avatarDiv);
-          
+        info.appendChild(photographerDiv);
+        info.appendChild(button);
+        info.appendChild(avatarDiv);  
         }    
     });
         const media = jsonObj["media"];
     //console.log(media);
 
     //créer variable pour compter les likes
-    //let likes = 0;
-
+    let likes = 0;
+    
     media.forEach(function(medium) {
       if (medium["photographerId"]==photographerId) {
 
         //ajouter le nombre de likes de medium à la variable des likes globale
-        //likes += medium["likes"];
-
-          let photos = document.getElementById("photos");
-          let mediaDiv = document.createElement("div");
-          mediaDiv.classList.add("mediadiv");
-            
-            //vérifier que medium["image"] existe, si oui on fait avec medium["image"] sinon avec medium["video"]
-            if (medium["image"]) {
-              mediaDiv.innerHTML = '<img src="./Sample Photos/' + currentPhotographer["name"] + '/' + medium["image"] + '"/> '+ '<div class="title">' + '<p>' + medium["title"] + '</p>' + '<p class="counter">' +  medium["likes"] + '</p>' + '<i class="fas fa-heart heart">' + '</i>' + '</div>';
-              photos.appendChild(mediaDiv);
-            
-            }
-            else if (medium["video"]) {
-              mediaDiv.innerHTML = '<video controls="controls" preload="auto" src="./Sample Photos/' + currentPhotographer["name"] + '/' + medium["video"] + '" />' ;
-              photos.appendChild(mediaDiv);
-            }
-
-          
-         
-
-            //<img src="./Sample Photos/Ellie Rose/Architecture_Connected_Curves.jpg"></img>
-            //<video controls="controls" poster="image.jpg" preload="auto">
-            //<source src="./Sample Photos/Mimi Keel/Animals_Wild_Horses_in_the_mountains.mp4" type="video/mp4"/>
-            //video Ellie</video>
-            //<i class="fas fa-heart"></i>         
+        likes += medium["likes"];
+        
+        let photos = document.getElementById("photos");
+        let mediaDiv = document.createElement("div");
+        mediaDiv.classList.add("mediadiv");
+    
+        //vérifier que medium["image"] existe, si oui on fait avec medium["image"] sinon avec medium["video"]
+        if (medium["image"]) {
+          mediaDiv.innerHTML = '<img src="./Sample Photos/' + currentPhotographer["name"] + '/' + medium["image"] + '"/>' + '<div class="title">' + '<p>' + medium["title"] + '</p>' + '<p class="counter">' +  medium["likes"] + '</p>' + '<i class="fas fa-heart heart">' + '</i>' + '</div>';
+          photos.appendChild(mediaDiv);
         }
+        else if (medium["video"]) {
+          mediaDiv.innerHTML = '<video controls="controls" preload="auto" src="./Sample Photos/' + currentPhotographer["name"] + '/' + medium["video"] + '"/>' + '<div class="title">' + '<p>' + medium["title"] + '</p>' + '<p class="counter">' +  medium["likes"] + '</p>' + '<i class="fas fa-heart heart">' + '</i>' + '</div>' ;
+          photos.appendChild(mediaDiv);
+        }      
+      }
     })
-        // event listener on the heart icon in order to count likes
-        let hearts = document.querySelectorAll(".heart");
-        //let counter = document.querySelectorAll(".counter");
-        hearts.forEach(heart => {
-          heart.addEventListener('click', function(e) {
-          let counter = e.target.previousSibling;
-          counter.innerHTML = parseInt(counter.innerHTML) + 1;
+    // event listener on the heart icon in order to count likes
+    let hearts = document.querySelectorAll(".heart");
 
-          //sélectionner compteur global et ajouter 1 de la même manière
- 
-        })
+    hearts.forEach(heart => {
+      heart.addEventListener('click', function(e) {
+      let counter = e.target.previousSibling;
+      counter.innerHTML = parseInt(counter.innerHTML) + 1;
+      //sélectionner compteur global et ajouter 1 de la même manière
+      let allLikes = likes; 
+      allLikes.innerHTML =  parseInt(allLikes.innerHTML) + 1;  
+      console.log(allLikes);
       })
-        //total likes and price
-        let totalLikes = document.getElementById("total-likes");
-        let likesDiv = document.createElement("div");
-        likesDiv.classList.add("likesdiv");
-        //likesDiv.innerHTML = '<p>' + '' + '</p>' + '<i id="heart" class="fas fa-heart">' + medium["price"] + '/' + 'jour';
-        totalLikes.appendChild(likesDiv);
+    })
+
+    //total likes and price
+    let totalLikes = document.getElementById("total-likes");
+    let likesDiv = document.createElement("div");
+    likesDiv.classList.add("likesdiv");
+    likesDiv.innerHTML = '<p>' + '' + '</p>' + likes  + '<i class="fas fa-heart heart">' + '/' + 'jour';
+    totalLikes.appendChild(likesDiv);
   })
 
-  //fill-in form launch
+//fill-in form launch
 
 //DOM elements
 const formBackground = document.querySelector(".background");
